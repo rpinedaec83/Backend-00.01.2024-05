76771b9abda26456d23eb87cbd22112e3ee4fc24
Elmer Silva <elmer.silva.uriarte@gmail.com>
2024-06-10 20:37:05 -0500

    Agrege boton 4

--- Semana03Sesion01/SV43927702/script.js
+++ Semana03Sesion01/SV43927702/script.js
@@ -1,3 +1,6 @@
 document.getElementById("btn3").addEventListener("click", function(){
     alert("Hola desde el tercer boton")
 })
+document.getElementById("btn4").addEventListener("click",function(){
+    alert("hi mundo")
+})
