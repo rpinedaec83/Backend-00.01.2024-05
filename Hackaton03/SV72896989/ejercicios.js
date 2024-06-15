ba1ec803098befad3daf17acb6c4bb2d1c48c2c7
Nicole Zuzunaga <zponce@gmail.com>
2024-06-15 15:03:21 -0500

    Ejercicio 1 resuelto

---
+++ Hackaton03/SV72896989/ejercicios.js
@@ -0,0 +1,10 @@
+function ejercicio1(){
+
+    let numero = parseInt(prompt("Escribe un numero"))
+    if (numero > 99 && numero < 1000){
+        alert("Este numero tiene 3 digitos")
+    }else{
+        alert("Este numero no tiene 3 digitos")
+    }
+}
+
