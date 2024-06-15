2be6f180077ff53944fcddecad0f23d3e0239c59
Nicole Zuzunaga <zponce@gmail.com>
2024-06-15 15:16:56 -0500

    Creación de botones para cada ejercicio

--- Hackaton03/SV72896989/ejercicios.js
+++ Hackaton03/SV72896989/ejercicios.js
@@ -8,3 +8,160 @@ function ejercicio1(){
     }
 }
 
+function ejercicio2(){
+
+    
+
+}
+
+function ejercicio3(){
+
+}
+
+function ejercicio4(){
+
+}
+
+function ejercicio5(){
+
+}
+
+function ejercicio6(){
+
+}
+
+function ejercicio7(){
+
+}
+
+function ejercicio8(){
+
+}
+
+function ejercicio9(){
+
+}
+
+function ejercicio10(){
+
+}
+
+function ejercicio11(){
+
+}
+
+function ejercicio12(){
+
+}
+
+function ejercicio13(){
+
+}
+
+function ejercicio14(){
+
+}
+
+function ejercicio15(){
+
+}
+
+function ejercicio16(){
+
+}
+
+function ejercicio17(){
+
+}
+
+function ejercicio18(){
+
+}
+
+function ejercicio19(){
+
+}
+
+function ejercicio20(){
+
+}
+
+function ejercicio21(){
+
+}
+
+function ejercicio22(){
+
+}
+
+function ejercicio23(){
+
+}
+
+function ejercicio24(){
+
+}
+
+function ejercicio25(){
+
+}
+
+function ejercicio26(){
+
+}
+
+function ejercicio27(){
+
+}
+
+function ejercicio28(){
+
+}
+
+function ejercicio29(){
+
+}
+
+function ejercicio30(){
+
+}
+
+function ejercicio31(){
+
+}
+
+function ejercicio32(){
+
+}
+
+function ejercicio33(){
+
+}
+
+function ejercicio34(){
+
+}
+
+function ejercicio35(){
+
+}
+
+function ejercicio36(){
+
+}
+
+function ejercicio37(){
+
+}
+
+function ejercicio38(){
+
+}
+
+function ejercicio39(){
+
+}
+
+function ejercicio40(){
+
+}
