43c51d643552a6d072f42699ae9d6203d7039b00
Nicole Zuzunaga <zponce@gmail.com>
2024-06-22 15:53:17 -0500

    Ejercicio 1 del Reto 1 resuelto

--- Hackaton04/SV72896989/ejercicios.js
+++ Hackaton04/SV72896989/ejercicios.js
@@ -1,4 +1,8 @@
 function ejercicio1_reto1(){
 
-    
+    let a = parseFloat(prompt("Ingrese el primer numero:"));
+    let b = parseFloat(prompt("Ingrese el segundo numero:"));
+    let suma = a + b;
+    alert("La suma de los dos numeros es " + suma);
+
 }
