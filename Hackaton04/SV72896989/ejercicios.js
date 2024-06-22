de883e3b3651ff058e134b23c14f6970fd87cc7a
Nicole Zuzunaga <zponce@gmail.com>
2024-06-22 16:25:11 -0500

    Ejercicio 3 del Reto 1 resuelto

--- Hackaton04/SV72896989/ejercicios.js
+++ Hackaton04/SV72896989/ejercicios.js
@@ -1,7 +1,7 @@
 function ejercicio1_reto1(){
 
-    let a = parseFloat(prompt("Ingrese el primer numero:"));
-    let b = parseFloat(prompt("Ingrese el segundo numero:"));
+    let a = parseFloat(prompt("Ingrese el primer numero"));
+    let b = parseFloat(prompt("Ingrese el segundo numero"));
     let suma = a + b;
     alert("La suma de los dos numeros es " + suma);
 
@@ -14,4 +14,26 @@ function ejercicio2_reto1(){
     let potencia = Math.pow(base, exponente);
     alert("La potencia " + base + " elevado a la " + exponente + " es " + potencia)
 
+}
+
+function ejercicio3_reto1(){
+
+    // Como quiero que el usuario ingrese los numeros cuyos cubos se sumaran, pedire que los separe por comas para que ingrese cuantos desee
+    let input = prompt("Ingrese los numeros separados por comas");
+
+    // Ahora convertire el string en un array de numeros
+    let numbers = input.split(",").map(num => Number(num.trim()));
+  
+    // Creo la función para el calculo de la suma de los cubos
+    const sumOfCubes = (...numbers) => {
+      return numbers.reduce(
+        (accumulator, currentValue) => accumulator + Math.pow(currentValue, 3),
+        0
+      );
+    };
+  
+    let resultado = sumOfCubes(...numbers);
+  
+    alert(`La suma de los cubos de los numeros ingresados es ${resultado}`);
+    
 }
