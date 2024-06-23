36769c20198bc5e41af3eba2e6257009dbbb2209
Elmer Silva <elmer.silva.uriarte@gmail.com>
2024-06-23 09:12:36 -0500

    Finalizzacion de Hackton4

--- Hackaton04/SV43927702/hack4.js
+++ Hackaton04/SV43927702/hack4.js
@@ -101,7 +101,96 @@ function primerIndiceUltimo(palabra, caracter) {
     return [primerIndice, ultimoIndice];
 }
   
+function objectToArray(obj) {
+    return Object.entries(obj);
+}
+
+const getBudgets = (presupuesto = []) => {
+    const sumResult = presupuesto.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.capital,
+   
+      0
+    );
+   
+    return sumResult;
+  };
+  function nombreEstudiantes(estudiantes) {
   
+    if (!Array.isArray(estudiantes)) {
+        throw new Error("La entrada debe ser una matriz.");
+    }
+
+
+    return estudiantes.map(estudiante => {
+        if (estudiante && estudiante.name) {
+            return estudiante.name;
+        } else {
+            throw new Error("Cada objeto de estudiante debe tener una propiedad 'name'.");
+        }
+    });
+}
+function objectToArray(obj) {
+    return Object.entries(obj);
+}
+function sumaCuadrados(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i * i;
+    }
+    return sum;
+}
+function multiplicarElementos(arr) {
+    const elementos = arr.length;
+    return arr.map(value => value * elementos);
+}
+function arrayAtras(n) {
+    const result = [];
+    for (let i = n; i >= 0; i--) {
+        result.push(i);
+    }
+    return result;
+}
+
+function diferenciaMaxMin(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    return max - min;
+}
+function filtraNumeros(arr) {
+    return arr.filter(item => Number.isInteger(item));
+}
+function repeat(valor, cantidad) {
+  
+    return Array(cantidad).fill(valor);
+}
+function reemplazarVocales(cadena, vocal) {
+    const vocales = /[aeiouAEIOU]/g;
+    return cadena.replace(vocales, vocal);
+}
+
+function buscaNemo(sentence) {
+    const palabras = sentence.split(" ");
+    const posicion = palabras.findIndex(palabras => palabras === "Nemo") + 1;
+    
+    if (posicion > 0) {
+        return `se encontro a nemo en : ${posicion}!`;
+    } else {
+        return "no se encontro a nemo :(";
+    }
+}
+
+function capLast(str) {
+    const words = str.split(" ");
+    const capitalizedWords = words.map(word => {
+        if (word.length > 0) {
+            const lastIndex = word.length - 1;
+            return word.slice(0, lastIndex) + word[lastIndex].toUpperCase();
+        } else {
+            return word;
+        }
+    });
+    return capitalizedWords.join(" ");
+}
 
 document.getElementById("Ejercicio1").addEventListener("click", (e) => {
     e.preventDefault();
@@ -224,4 +313,100 @@ document.getElementById("reto9").addEventListener("click", (e) => {
     let caracter=prompt("ingrese Caracter a Buscar")
    alert (primerIndiceUltimo(palabra,caracter))
 })
+document.getElementById("reto10").addEventListener("click", (e) => {
+    e.preventDefault();
+    const obj = { a: 1, b: 2 };
+const array = objectToArray(obj);
+alert(array);
+})
 
+document.getElementById("reto11").addEventListener("click", (e) => {
+    e.preventDefault();
+    const resultBudgets = getBudgets([
+        { name: "John", age: 21, capital: 23000 },
+        { name: "Steve", age: 32, capital: 40000 },
+        { name: "Martin", age: 16, capital: 2700 },
+      ]);
+      alert(resultBudgets)
+})
+
+document.getElementById("reto12").addEventListener("click", (e) => {
+    e.preventDefault();
+    const students = [
+        { name: 'Juan', age: 20 },
+        { name: 'Ana', age: 22 },
+        { name: 'Carlos', age: 21 }
+    ];
+      alert(nombreEstudiantes(students));
+})
+document.getElementById("reto13").addEventListener("click", (e) => {
+    e.preventDefault();
+    const exampleObject = {
+        likes: 2,
+        dislikes: 3,
+        followers: 10
+    };
+      alert(objectToArray(exampleObject));
+})
+
+document.getElementById("reto14").addEventListener("click", (e) => {
+    e.preventDefault();
+   let n1 =parseInt(prompt("ingrese Numero"))
+      alert(sumaCuadrados(n1));
+})
+
+document.getElementById("reto15").addEventListener("click", (e) => {
+    e.preventDefault();
+    let datos1=prompt("ingrese valores separados por ,")
+    let cadena1 = datos1.split(",").map(str => str.trim());
+    let numeros1= cadena1.map(Number);
+    alert(multiplicarElementos(numeros1));
+})
+document.getElementById("reto16").addEventListener("click", (e) => {
+    e.preventDefault();
+    let dato=parseInt(prompt("ingrese Numero"))
+    
+    alert(arrayAtras(dato));
+})
+document.getElementById("reto17").addEventListener("click", (e) => {
+    e.preventDefault();
+    let datos1=prompt("ingrese valores separados por ,")
+    let cadena1 = datos1.split(",").map(str => str.trim());
+    let numeros1= cadena1.map(Number);
+    alert(diferenciaMaxMin(numeros1));
+})
+document.getElementById("reto18").addEventListener("click", (e) => {
+    e.preventDefault();
+ 
+    
+    alert(filtraNumeros([1, "a", "b", 0, 15]));
+})
+document.getElementById("reto19").addEventListener("click", (e) => {
+    e.preventDefault();
+    let valor=prompt("ingrese elemnto a repetir")
+    let can=parseInt(prompt("ingrese cantidad de repeticion"))
+    
+    alert(repeat(valor,can));
+})
+document.getElementById("reto20").addEventListener("click", (e) => {
+    e.preventDefault();
+    let cadena=prompt("ingrese cadena")
+    let vocal=prompt("ingrese vocal a reemplazar")
+    
+    alert(reemplazarVocales(cadena,vocal));
+})
+document.getElementById("reto21").addEventListener("click", (e) => {
+    e.preventDefault();
+    let cadena=prompt("ingrese cadena")
+ 
+    
+    alert(buscaNemo(cadena));
+})
+
+document.getElementById("reto22").addEventListener("click", (e) => {
+    e.preventDefault();
+    let cadena=prompt("ingrese cadena")
+ 
+    
+    alert(capLast(cadena));
+})
