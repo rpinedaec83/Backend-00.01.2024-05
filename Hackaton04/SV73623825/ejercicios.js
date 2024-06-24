b141c86245d91bb798c0f1103752522b60597fbe
edrivera <daviduniforever@gmail.com>
2024-06-23 22:58:10 -0500

    Hackaton04 terminado SV73623825

--- Hackaton04/SV73623825/ejercicios.js
+++ Hackaton04/SV73623825/ejercicios.js
@@ -236,36 +236,151 @@ function ejercicio11_2(){
       ]);
       alert(resultBudgets)
 }
+//Cree una función que tome una matriz de estudiantes y devuelva una matriz de nombres de estudiantes.
 function ejercicio12_2(){
-
+    function getStudentNames(students=[]) {
+        return students.map(student => student.name);
+    }
+    const resultStudenNames = getStudentNames([
+        { name: "John"},
+        { name: "Steve"},
+        { name: "Martin"},
+    ]);
+    alert(JSON.stringify(resultStudenNames))
 }
+//Escriba una función que convierta un objeto en una matriz de claves y valores.
 function ejercicio13_2(){
-
+    const objectToArray = (obj = {}) => {
+        const result = [];
+       
+        for (const clave in obj) {
+          result.push([clave, obj[clave]]);
+        }
+        return result;
+      };
+    const result= objectToArray({
+        likes: 2,
+        dislikes: 3,
+        followers: 10
+      })
+    alert(JSON.stringify(result))  
 }
+//Cree una función donde, dado el número n, devuelva la suma de todos los números cuadrados  incluyendo n.
 function ejercicio14_2(){
+    let num = parseInt(prompt("Ingrese un numero"))
+    const sumacuadrado = (num)=>{
+        let suma=0
+        for (let index = 1; index <= num; index++){
+            suma=suma+Math.pow(index,2)
+        }
+        return suma;   
+    }
 
+    const suma=sumacuadrado(num)
+    alert("La suma de cuadrado es "+suma)
 }
+//Cree una función para multiplicar todos los valores en una matriz por la cantidad de valores en la matriz dada
 function ejercicio15_2(){
-
+    const multiplyByLength = (arr=[]) => {
+        const length = arr.length; 
+        return arr.map(value => value * length); 
+    };
+    let data=[]
+    let cantidad  = parseInt(prompt("Escribe la cantidad de numeros que va a ingresar"));
+    for (let index = 0; index < cantidad; index++) {
+        data.push(parseInt(prompt("Escribe un numero")));
+    }
+    alert(multiplyByLength(data))
 }
+//Cree una función que tome un número como argumento y devuelva una matriz de números contando desde este número a cero.
 function ejercicio16_2(){
-
+    const countdown = (num) => {
+        const result = [];
+        for (let i = num; i >= 0; i--) {
+            result.push(i);
+        }
+        return result;
+    };
+    let num = parseInt(prompt("Ingrese un numero"))
+    alert(countdown(num))
 }
+//Cree una función que tome una matriz y devuelva la diferencia entre los números más grandes y más pequeños.
 function ejercicio17_2(){
-
+    let data=[]
+    let cantidad  = parseInt(prompt("Escribe la cantidad de numeros que va a ingresar"));
+    for (let index = 0; index < cantidad; index++) {
+        data.push(parseInt(prompt("Escribe un numero")));
+    }
+    function max_min(matriz=[]) {
+        let data=0
+        data = (Math.max(...matriz))-(Math.min(...matriz))
+        return data
+    }
+    alert("La diferencia entre el maximo y minimo valor es "+max_min(data))
 }
+//Cree una función que filtre las cadenas de una matriz y devuelva una nueva matriz que solo contenga enteros.
 function ejercicio18_2(){
-
+    let data=[1, 2, 3, "x", "y", 10]
+    const arr = (data=[])=>{
+        let data_nuevo=[]
+        for (let index = 0; index < data.length; index++) {
+            if (typeof (data[index])!=='string') {
+                data_nuevo.push(data[index])
+            } 
+        }
+        return data_nuevo
+    }
+    alert(arr(data))
 }
+/*Cree una función que tome dos argumentos (elemento, tiempos). 
+El primer argumento (elemento) es el elemento que necesita repetirse, mientras que el segundo argumento (veces) 
+es la cantidad de veces que se debe repetir el elemento. Devuelve el resultado en una matriz.*/
 function ejercicio19_2(){
-
+    const repeatElement = (num, rep) => {
+        const result = [];
+        for (let i = 0; i < rep; i++) {
+            result.push(num);
+        }
+        return result;
+    };
+    let num = parseInt(prompt("Escribe un numero"))
+    let rep = parseInt(prompt("La cantidad que desee q se repita"))
+    alert(repeatElement(num,rep))
 }
+//Escriba una función, .vreplace () que extienda el prototipo de 
+//cadena reemplazando todas las vocales en una cadena con una vocal especificada.
 function ejercicio20_2(){
-
+    String.prototype.vreplace = function(vocal) {
+        const vocales = /[aeiouAEIOU]/g;
+        return this.replace(vocales, vocal);
+    };
+    let texto = (prompt("Escribe un texto"))
+    let vocal = (prompt("Escribe la vocal que desee colocar"))
+    alert(texto.vreplace(vocal));
 }
+//Te dan una cadena de palabras. Debe encontrar la palabra "Nemo" y 
+//devolver una cadena como esta: "¡Encontré a Nemo en [el orden de la palabra que encuentra nemo]!".
 function ejercicio21_2(){
-
+    const buscador = (sentence,palabra) => {
+        const words = sentence.split(' ');
+        const position = words.indexOf(palabra) + 1;
+        if (position > 0) {
+            return `¡Encontré a "${palabra}" en ${position}!`;
+        } else {
+            return `"${palabra}" no está en la cadena.`;
+        }
+    };
+    let texto = (prompt("Escribe un texto"))
+    let palabra = (prompt("Escribe la palabra que desee encontrar"))
+    alert(buscador(texto,palabra));
 }
+//Cree una función que capitalice la última letra de cada palabra.
 function ejercicio22_2(){
-
+    const capitalice = (texto) =>{
+        const texto1SinUltimaLetra = texto.slice(0, -1)
+        const ultimaLetraMayuscula = texto.charAt(texto.length - 1).toUpperCase();
+        return texto1SinUltimaLetra+ultimaLetraMayuscula
+    }
+    let texto = (prompt("Escribe un texto"))
+    alert(capitalice(texto))
 }
