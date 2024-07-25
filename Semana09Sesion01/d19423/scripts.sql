d39fa498129f481bd58dd8d22d223fc58068e9e2
Roberto Pineda <rpineda@x-codec.net>
2024-07-24 22:21:53 -0500

    scripts sql

--- Semana09Sesion01/d19423/scripts.sql
+++ Semana09Sesion01/d19423/scripts.sql
@@ -90,3 +90,7 @@ SELECT distinct city FROM Customer;
 
 SELECT * FROM Customer
 WHERE Country <> 'Germany'
+CREATE VIEW [Brazil Customers] AS
+SELECT CustomerName, ContactName
+FROM Customers
+WHERE Country = 'Brazil';
--- Semana09Sesion02/d19423/callSP.sql
+++ Semana09Sesion02/d19423/callSP.sql
@@ -1,5 +1,11 @@
 call crud_color(2,'Gris',null, null, 1);
 call crud_color(1,null,null,null, null);
-call crud_color(3,'Atigrado', 0, 3, 1);
+call crud_color(3,'Negro', 1, 2, 1);
 call crud_color(4,null, null, 2, 1);
 call crud_color(3,null, 1, 2, 1);
+
+select * from color;
+select * from color_bck;
+
+
+delete from color where id=4;
