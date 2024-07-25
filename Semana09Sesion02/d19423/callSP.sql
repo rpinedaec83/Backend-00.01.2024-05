db3b46afcb79d1cf0001c997b931c03d456a3795
Roberto Pineda <rpineda@x-codec.net>
2024-07-24 21:26:31 -0500

    scripts sql

--- Semana09Sesion02/d19423/callSP.sql
+++ Semana09Sesion02/d19423/callSP.sql
@@ -1,2 +1,5 @@
-call crud_color(2,'Negro',1);
-call crud_color(1,null,null);
+call crud_color(2,'Gris',null, null, 1);
+call crud_color(1,null,null,null, null);
+call crud_color(3,'Atigrado', 0, 3, 1);
+call crud_color(4,null, null, 2, 1);
+call crud_color(3,null, 1, 2, 1);
