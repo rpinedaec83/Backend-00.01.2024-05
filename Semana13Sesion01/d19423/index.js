b31d1086a9aebb0d6f42c07484385b934c9b7e1c
Roberto Pineda <rpineda@x-codec.net>
2024-08-19 20:39:47 -0500

    Clase 19982024 primera parte

---
+++ Semana13Sesion01/d19423/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res)=>{
+    res.send("Hola desde el post")
+})
+
+app.put('/', (req, res)=>{
+    res.send("Hola desde el put")
+})
+
+app.delete('/', (req, res)=>{
+    res.send("Hola desde el delete")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
